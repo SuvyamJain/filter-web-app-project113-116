23be2cfe081cf2c0e9e37dac27faf377a8a37188
SuvyamJain <84795180+SuvyamJain@users.noreply.github.com>
2022-01-06 20:28:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+   }
+   function preload()
+   {
+     
+   }
+   function draw() {
+     
+   }
+   function take_pic()
+ 
+ {
+    save("JOKER_IMAGE.png");
+ }
+ 
